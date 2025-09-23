d2485b06eeaae7411297b5530da528074a2179e9
kanishk-28 <kanishk28114@gmail.com>
2025-09-23 13:55:06 +0000

    variables & datatypes

---
+++ basics/Data_Types.js
@@ -0,0 +1,27 @@
+"use strict";   //Treat all JS code as newer version
+ //alert(3+3) //WE are using NodeJs, not Browser
+
+ /*
+
+ COMMON DATATYPE IN JS:-
+--Primitive Data_types--
+ number-> range=2^53
+ bigint -> for bigger values
+ string -> " "
+ boolean -> true/false
+ null -> stand alone value ,It is a object in JS.
+ undefined -> e.g. let state;
+ symbol -> used for uniqueness
+
+ */
+ //object 
+let age=6
+let name="kanishk"
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const id=5598974;
+let email="kanishk@88gmail.com";
+var code=664646
+city="Jaipur"
+let state;
+// DO NOT USE var  BECAUSE OF ISSUE IN  BLOCK SCOPE & FUNCTIONAL SCOPE
+email="dkf@gfg.com"
+city="Surat"
+console.table([email,id,city,state])
+console.log(id);
