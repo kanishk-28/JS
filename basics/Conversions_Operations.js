b55b438f5a53455ad06ad9c8e6d1460e6de18439
kanishk-28 <kanishk28114@gmail.com>
2025-09-23 14:24:46 +0000

    Conversion of DataTypes

---
+++ basics/Conversions_Operations.js
@@ -0,0 +1,16 @@
+let score="95abc";
+//let score=null
+//let score = undefined
+//let score = true
+//let score = false
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+/*
+"95"-> 95
+"95abc"-> NaN (Not a Number)
+true -> 1 / false -> 0
+*/
