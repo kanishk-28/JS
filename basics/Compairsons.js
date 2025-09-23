632f240bf59e7beb0a4d34920b5a2bdaac27ca3d
kanishk-28 <kanishk28114@gmail.com>
2025-09-23 15:09:31 +0000

    Comparisons

---
+++ basics/Compairsons.js
@@ -0,0 +1,7 @@
+console.log("2"==2)
+console.log(null>0);
+
+console.log(null>=0); //true
+console.log(null==0); //false
+
+console.log("2"===2); //it's  a strict check ,it checks the datatype and value of the given input
