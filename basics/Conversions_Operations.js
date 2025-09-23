69fdd6c80f6eaa96846df6f1646b081b2b2ec5b1
kanishk-28 <kanishk28114@gmail.com>
2025-09-23 14:43:38 +0000

    conversion & operations

--- basics/Conversions_Operations.js
+++ basics/Conversions_Operations.js
@@ -1,16 +1,32 @@
+// ---- CONVERSIONS --------
 let score="95abc";
 //let score=null
 //let score = undefined
 //let score = true
 //let score = false
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
 /*
 "95"-> 95
 "95abc"-> NaN (Not a Number)
 true -> 1 / false -> 0
-*/
+*/ 
+// ---------OPERATIONS----------- 
+   
+let value =8
+let negvalue = -value
+console.log(negvalue);
+ 
+let str1 ="hi"
+let str2 = " kanishk"
+console.log(str1+str2);
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"9");
+
+
