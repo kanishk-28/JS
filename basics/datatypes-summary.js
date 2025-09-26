9752a7b971af9fd3f1c9ab95e1cd6c78ab879e4d
kanishk-28 <kanishk28114@gmail.com>
2025-09-26 12:43:26 +0000

    datatype-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+//JS is a dynamically typed language.
+
+/*      Data Types --> 
+1.Primitive      
+      7 types --- String, Number, null, Boolean, undefined, Symbol, BigInt
+*/
+
+const score=155;
+const scoreValue =14.3
+const loggedIN =false
+const outTemp=null
+let userEmail =undefined
+
+const id =Symbol("465")
+const ID = Symbol("465")
+ console.log(id===ID);
+
+ const bigNumber=557446465446446n
+ 
+//2.Referance(Non-Primitive)
+   // 3 types --- Array, Functions, Objects
+const hero=["PAPA","ms","sourav"];
+console.log(hero);
+
+let muObj= {
+    name:"Kanishk",  //this is object
+    age:21,
+}
+
+const Myfun= function(){
+    console.log("Hello");
+    
+}
+
